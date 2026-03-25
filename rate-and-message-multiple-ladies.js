ae31a32ae45423ff5ac6430e1df898f16b473bd4
TheOuroboros01 <the.ouroboros1@yahoo.com>
2026-03-26 01:36:04 +0530

    Update rate-and-message-multiple-ladies.js

--- rate-and-message-multiple-ladies.js
+++ rate-and-message-multiple-ladies.js
@@ -183,7 +183,7 @@ module.exports = async function runRateAndMessageMultipleLadies(page, tierConfig
     console.log('✅ No excluded ladies detected automatically.');
   }
 
-  await page.waitForTimeout(1800000); //delay or pause for manual verification
+  await page.waitForTimeout(5000); //delay or pause for manual verification
 
   // ─────────────────────────────────────────────
   // ✅ FINAL FILTER
